329eae833ddffa70d625bca605ab6d9af38a077c
DESKTOP-4RG4IVB\ruixi <ruixiong8@hotmail.com>
2022-11-05 20:48:26 +0800

    update

--- user-service/index.js
+++ user-service/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 
+const corsConfig = {
+    credentials: true,
+    origin: true,
+  };
+
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
+app.use(cors(corsConfig)) // config cors so that front-end can use
 app.options('*', cors())
 
 import { createUser, loginUser, logoutUser, deleteUser, pwChange } from './controller/user-controller.js';
