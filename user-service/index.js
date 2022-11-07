380fd76bcbc853272d58782f9d746255415dc4cd
Lim Junxue <60027105+LimJunxue@users.noreply.github.com>
2022-11-07 18:36:14 +0800

    Update ACAO

--- user-service/index.js
+++ user-service/index.js
@@ -37,7 +37,7 @@ router.post('/pwChange', pwChange)
 
 app.use('/api/user', router).all((_, res) => {
     res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', 'https://user-service-xkpqea35pq-as.a.run.app')
+    res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
 app.listen(8000, () => console.log('user-service listening on port 8000'));
