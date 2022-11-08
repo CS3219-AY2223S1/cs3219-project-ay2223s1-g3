1e407da76f0f5f5f84a2ac15a343772d50755086
Xin Wei <65399872+xiinweii98@users.noreply.github.com>
2022-11-09 00:47:00 +0800

    Update repository.js

--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 //Set up mongoose connection
 import mongoose from 'mongoose';
 
+console.log(process.env.ENV);
+console.log(process.env.DB_CLOUD_URI);
 let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
