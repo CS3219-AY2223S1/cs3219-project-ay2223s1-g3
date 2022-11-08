38602994ebea0b8b01c9bead78135b4f4e085ac6
xiinweii98 <xiinweii98@gmail.com>
2022-11-09 06:04:45 +0800

    store blacklisted jwt to mongodb as well. reload redis with mongo data after crash

--- user-service/model/token-blacklist.js
+++ user-service/model/token-blacklist.js
@@ -1,5 +1,19 @@
 import redis from "redis";
 import "dotenv/config";
+import JwtModel from "./jwt-model.js";
+
+//Set up mongoose connection
+import mongoose from "mongoose";
+
+let mongoDB =
+    process.env.ENV == "PROD"
+        ? process.env.DB_CLOUD_URI
+        : process.env.DB_LOCAL_URI;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const redisHost = process.env.REDIS_CLOUD_IP;
 const redisPort = process.env.REDIS_CLOUD_PORT;
@@ -15,7 +29,8 @@ const redisClient = redis.createClient({
 redisClient.on("error", (err) => {
     console.log(`Unable to create redis with error: ${err}`);
 });
-redisClient.on("connection", () => {
+redisClient.on("connect", () => {
+    reloadRedis();
     console.log("Connected to redis!");
 });
 
@@ -23,6 +38,8 @@ await redisClient.connect();
 
 export async function addToBlacklist(token, tokenExp) {
     try {
+        const tokenModel = new JwtModel({ token });
+        tokenModel.save();
         const token_key = `bl_${token}`;
         await redisClient.set(token_key, token);
         redisClient.expireAt(token_key, tokenExp);
@@ -37,6 +54,25 @@ export async function addToBlacklist(token, tokenExp) {
 }
 
 export async function inBlacklist(token) {
-    const inBlacklist = await redisClient.get(`bl_${token}`);
-    return inBlacklist;
+    await redisClient.get(`bl_${token}`, async (err, res) => {
+        if (err) {
+            reloadRedis();
+            return await redisClient.get(`bl_${token}`);
+        }
+        return res;
+    });
+}
+
+function reloadRedis() {
+    JwtModel.find({}, (err, jwts) => {
+        if (err) console.log("Unable to retrieve JWTs from MongoDB.");
+        jwts.map(async (jwt) => {
+            console.log(jwt);
+            let token = jwt.token;
+            if (!(await redisClient.get(`bl_${token}`))) {
+                let token_key = `bl_${token}`;
+                await redisClient.set(token_key, token);
+            }
+        });
+    });
 }
